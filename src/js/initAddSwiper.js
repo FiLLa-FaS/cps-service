b505f2dc989a16c5727f2244e81649e65d06d935
Yana <fillafas@yandex.ru>
2022-06-21 23:16:42 +0300

    swiper fix

--- src/js/initAddSwiper.js
+++ src/js/initAddSwiper.js
@@ -1,11 +1,30 @@
 export function initAddSwiper() {
-  if (document.documentElement.clientWidth < 650) {
-    const swiper = new Swiper('.swiper', {
-      direction: 'horizontal',
-      loop: false,
-      pagination: {
-        el: '.swiper-pagination',
-      },
-    })
-  }
+  const sliders = document.querySelectorAll('.swiper')
+
+  sliders.forEach((slider) => {
+    let mySwiper
+    function mobileSlider() {
+      if (window.innerWidth <= 650 && slider.dataset.mobile == 'false') {
+        mySwiper = new Swiper(slider, {
+          direction: 'horizontal',
+          loop: false,
+          pagination: {
+            el: slider.querySelector('.swiper-pagination'),
+          },
+        })
+
+        slider.dataset.mobile = 'true'
+      }
+
+      if (window.innerWidth > 650) {
+        slider.dataset.mobile = 'false'
+
+        if (slider.classList.contains('swiper-initialized')) {
+          mySwiper.destroy()
+        }
+      }
+    }
+    mobileSlider()
+    window.addEventListener('resize', mobileSlider)
+  })
 }
